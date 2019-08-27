e8d800d4bae99d35a47e5d3e5b0c87f26c67a8bf
NLSteveO <steveokeefe88@gmail.com>
2019-08-27 17:40:20 -0230

    Add index.js which creates and runs a new feathers server

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const cors = require('cors');
+
+const app = express(feathers());
+
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
+
+app.configure(express.rest());
+
+app.use(express.errorHandler());
+app.use(cors());
+
+const server = app.listen(4000);
+server.on('listening', () => console.log('Server is listening on http://localhost:4000'));
