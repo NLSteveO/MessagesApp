ad29069b4a025718aa3b6d13b3ea131e57251522
NLSteveO <steveokeefe88@gmail.com>
2020-02-28 07:31:04 -0330

    Creat Messages service and register it on the path /messages
    The Messages service allows you to get all messages, retrieve a specific message, create a message with a given string, patch a specified message with a new given string, and delete a specified message

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,8 @@ const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
 const cors = require('cors');
 
+const Messages = require('./Messages');
+
 const app = express(feathers());
 
 app.use(express.json());
@@ -14,6 +16,8 @@ app.configure(express.rest());
 
 app.use(express.errorHandler());
 app.use(cors());
+
+app.use('messages', new Messages());
 
 const server = app.listen(4000);
 server.on('listening', () => console.log('Server is listening on http://localhost:4000'));
